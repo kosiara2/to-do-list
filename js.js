f0823a2076125cf630a2ec904a259920a92200d9
kosiara2 <patrycja.kosierkiewicz@o2.pl>
2023-05-19 21:44:49 +0200

    editing tasks

--- js.js
+++ js.js
@@ -1,24 +1,30 @@
 let inputToDo; // place where user inupting task to do
 let addBtn; // button for adding task to list
 
-let popupInput;
-let confirmBtn;
-let cancelBtn;
 let tools;
 let error;
 let ulList; // list with tasks
 let newTask;
+
+let popup;
+let popupInfo;
+let popupInput;
+let acceptBtn;
+let cancelBtn;
 
 //html dom all elements
 const DOMElements = () => {
   inputToDo = document.querySelector(".todo-input");
   addBtn = document.querySelector(".btn-add");
   tools = document.querySelector(".tools");
-  popupInput = document.querySelector(".popup-input");
-  confirmBtn = document.querySelector(".accept");
-  cancelBtn = document.querySelector(".cancel");
   error = document.querySelector(".error-info");
   ulList = document.querySelector(".todoList ul");
+
+  acceptBtn = document.querySelector(".accept");
+  cancelBtn = document.querySelector(".cancel");
+  popupInput = document.querySelector(".popup-input");
+  popup = document.querySelector(".popup");
+  popupInfo = document.querySelector(".popup-info");
 };
 //main - all functions
 const main = () => {
@@ -29,6 +35,7 @@ const main = () => {
 const addNewTask = () => {
   if (inputToDo.value !== "") {
     newTask = document.createElement("li");
+
     ulList.appendChild(newTask);
     newTask.textContent = inputToDo.value;
     createToolsArea();
@@ -59,9 +66,29 @@ const createToolsArea = () => {
   toolPanel.append(completeBtn, editBtn, deleteBtn);
 };
 
+const checkClick = (e) => {
+  if (e.target.matches(".complete")) {
+    e.target.closest("li").classList.toggle("completed");
+  } else if (e.target.matches(".edit")) {
+    editingTask();
+  } else if (e.target.matches(".delte")) {
+    console.log("delte");
+  }
+};
+
+const editingTask = () => {
+  popup.style.display = "flex";
+};
+
+const cancelEditing = () => {
+  popup.style.display = "none";
+};
+
 //Events
 const DOMEvents = () => {
   addBtn.addEventListener("click", addNewTask);
+  ulList.addEventListener("click", checkClick);
+  cancelBtn.addEventListener("click", cancelEditing);
 };
 
 // content loaded
